1c085e6934ae3c240939e20814e7e231cccd4b85
Amanda Wong <Amanda Wong>
2016-03-05 15:25:15 -0800

    Add front-page images

--- wp-content/themes/leredbread/front-page.php
+++ wp-content/themes/leredbread/front-page.php
@@ -12,6 +12,36 @@ get_header(); ?>
             <section class="hero">
                 <h1>Baked to perfection.</h1>
             </section>
+            <section class="product-info">
+                <div>
+                    <div class="product-img-wrapper">
+                        <img src="images/bread.png">
+                    </div>
+                    <h4>Bread</h4>
+                    <p>Bread is the ultimate comfort food! Our lust-worthy loaves will satisfy your most intense carb cravings.<span><a href="#">See More...</a></span></p>
+                </div>
+                <div>
+                    <div class="product-img-wrapper">
+                        <img src="assets/images/coffee.png">
+                    </div>
+                    <h4>Coffee</h4>
+                    <p>Start your day with decadent cup o' joe, or pop in for a mid-afternoon pick me up. Caffeine FTW.<span><a href="#">See More...</a></span></p>
+                </div>
+                <div>
+                    <div class="product-img-wrapper">
+                        <img src="assets/images/pretzels.png">
+                    </div>
+                    <h4>Pretzel</h4>
+                    <p>Our soft-pretzels are available in a variety of flavours. When they taste this good, one is never enough!<span><a href="#">See More...</a></span></p>
+                </div>
+                <div>
+                    <div class="product-img-wrapper">
+                        <img src="assets/images/sweets.png">
+                    </div>
+                    <h4>Sweets</h4>
+                    <p>Our sinfully delicious sweets are simply irresistible! These gourmet treats will keep you coming back.<span><a href="#">See More...</a></span></p>
+                </div>
+            </section>
 
 
 		</main><!-- #main -->
